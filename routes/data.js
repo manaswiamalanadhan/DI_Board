fe5086b5ca17796132dec908fd48ff608ab12416
Debanshu Sinha <sinhadebanshu@gmail.com>
2018-07-15 02:37:11 +0530

    adding cuisine area map

--- routes/data.js
+++ routes/data.js
@@ -1,13 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../utils/mongo');
+const express = require('express');
+const router = express.Router();
+const db = require('../utils/mongo');
+const geoHashConverter = require('../utils/geohash-to-latlon');
 
-router.get('/all', function(req, res) {
-  var collection = db.get().collection('items_metadata');
+/**
+ * To test code
+ */
+router.get('/experiments', (req, res) => {
+    const collection = db.get().collection('items_metadata');
 
-  collection.find({"dish_family": "pav"}).toArray(function(err, docs) {
-    res.send(docs);
-  })
-})
+    collection.find({"dish_family": "pav"}).toArray((err, docs) => {
+        res.send(docs);
+    })
+});
+
+/**
+ * For cuisine aggregated by area points
+ */
+router.get('/cuisine_geohash', (req, res) => {
+    const collection = db.get().collection('cuisine_geohash');
+
+    collection.find({}).toArray((err, docs) => {
+        res.send(docs.map( doc => {
+            // console.log('converting', data._id);
+            doc.location = geoHashConverter(doc._id);
+            delete doc._id;
+            const cuisines = doc.cuisines.reduce((cum, cur) => { //cumulative, current
+                if(cur in cum)
+                    cum[cur]++;
+                else
+                    cum[cur]=1;
+                return cum;
+            }, {});
+            delete doc.cuisines;
+            return Object.assign(doc, cuisines);
+        }));
+    });
+});
 
 module.exports = router;
